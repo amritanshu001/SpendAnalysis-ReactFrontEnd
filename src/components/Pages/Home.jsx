856878115b310e0318f9e14ce3152133874a5bec
amritanshu001 <91420206+amritanshu001@users.noreply.github.com>
2022-12-07 11:32:13 +0530

    Home page content

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -5,22 +5,27 @@ import React from "react";
 
 const homeContent = [
   {
+    id: "01",
     title: "Login/Register",
-    desc: "User can login into the system with the registered credentials.",
+    desc: "User can register themselves on the site and set their password OR can login into the system with the registered credentials.",
   },
   {
+    id: "02",
     title: "Manage Bank Accounts",
     desc: "User can add / removed their bank accounts in the database.",
   },
   {
+    id: "03",
     title: "Upload Statement",
     desc: "Users can select their bank account and upload their statements in excel format into the system.",
   },
   {
+    id: "04",
     title: "Spend Analysis",
     desc: "Users can generate spend analysis on the uploaded statement for a specific account and a date period.",
   },
   {
+    id: "05",
     title: "Add Bank Details (Admin Only)",
     desc: "Admin users can maintain the Bank names and the format of excel statement to assist the system in uploading the data into the system.",
   },
@@ -29,7 +34,7 @@ const homeContent = [
 const Home = (props) => {
   const mapHomeContent = (content) => {
     return (
-      <div className={styles.row}>
+      <div className={styles.row} key={content.id}>
         <span className={styles["bold-span"]}>{content.title}</span>
         <span className={styles["normal-span"]}>{content.desc}</span>
       </div>
