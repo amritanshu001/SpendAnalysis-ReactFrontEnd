a840f34380911bc01af9b63a3c935d3d1b8636b4
Amritanshu <91420206+amritanshu001@users.noreply.github.com>
2023-05-28 13:07:34 +0530

    Update endpoint.js to correct  the url

--- src/endpoint.js
+++ src/endpoint.js
@@ -1,8 +1,8 @@
 let apiURL;
-if (navigator.platform === "Win32") {
-  apiURL = "http://127.0.0.1:5001";
-} else {
+// if (navigator.platform === "Win32") {
+//   apiURL = "http://127.0.0.1:5001";
+// } else {
   apiURL = "https://analyzespends.onrender.com";
-}
-console.log(apiURL)
+// }
+// console.log(apiURL)
 export default apiURL;
