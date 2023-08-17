937745ceda3229955920064675d08ed2aa939bce
Amritanshu <91420206+amritanshu001@users.noreply.github.com>
2023-08-17 12:26:19 +0530

    correcting endpoint address

--- src/endpoint.js
+++ src/endpoint.js
@@ -1,8 +1,8 @@
 let apiURL;
 // if (navigator.platform === "Win32") {
-  apiURL = "http://127.0.0.1:5000";
+//   apiURL = "http://127.0.0.1:5000";
 // } else {
-// apiURL = "https://analyzespends.onrender.com";
+apiURL = "https://analyzespends.onrender.com";
 // }
 // console.log(apiURL)
 export default apiURL;
