d1cdde983f4b48219b8f89a4b34474d805d2cee0
Amritanshu <91420206+amritanshu001@users.noreply.github.com>
2023-12-12 18:35:07 +0530

    adding filters on spend chart

--- src/components/UI/Chart/SpendChart.jsx
+++ src/components/UI/Chart/SpendChart.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SpendChart.module.css";
 import createTrend from "trendline";
+import Select from "react-select";
 
 import {
   Chart as ChartJS,
@@ -34,7 +35,50 @@ const getMonthName = (monthNumber) => {
   return date.toLocaleString("default", { month: "long" });
 };
 
+const getMonthFromString = (mon) => {
+  return new Date(Date.parse(mon + " 1, 2023")).getMonth();
+};
+
 const SpendChart = (props) => {
+  const monthYears = props.chartData.map(
+    (chartItem) =>
+      getMonthName(chartItem.date.month) + "," + chartItem.date.year
+  );
+
+  const [fromDate, setFromDate] = useState(0);
+  console.log(fromDate);
+  const [toDate, setToDate] = useState(0);
+  const toDateOptions =
+    fromDate === 0
+      ? monthYears
+      : props.chartData
+          .filter((chartItem) => {
+            const monthNumber = getMonthFromString(
+              fromDate.label.split(",")[0]
+            );
+            const year = fromDate.label.split(",")[1];
+            if (chartItem.date.year > +year) {
+              return true;
+            }
+            if (chartItem.date.month >= monthNumber) {
+              return true;
+            } else {
+              return false;
+            }
+          })
+          .map(
+            (chartItem) =>
+              getMonthName(chartItem.date.month) + "," + chartItem.date.year
+          );
+
+  const fromDateChangeHandler = (event) => {
+    setFromDate(event);
+  };
+
+  const toDateChangeHandler = (event) => {
+    setToDate(event);
+  };
+
   const trendData = props.chartData
     .map((chartItem) => chartItem.closingBal)
     .map((balance, index) => {
@@ -43,9 +87,7 @@ const SpendChart = (props) => {
   const trend = createTrend(trendData, "x", "closingBal");
 
   const data = {
-    labels: props.chartData.map((chartItem) => {
-      return getMonthName(chartItem.date.month) + "," + chartItem.date.year;
-    }),
+    labels: monthYears,
     datasets: [
       {
         type: "line",
@@ -96,6 +138,34 @@ const SpendChart = (props) => {
     <div className={styles.chart}>
       <div>Spend Graph</div>
       <Chart type="bar" data={data} />
+      <div className={styles.dates}>
+        <div className={styles["date-select"]}>
+          <label>From Date</label>
+          <Select
+            className={styles.reactselect}
+            value={fromDate}
+            isClearable={true}
+            isSearchable={true}
+            onChange={fromDateChangeHandler}
+            options={monthYears.map((monthItem, indx) => {
+              return { value: indx, label: monthItem };
+            })}
+          />
+        </div>
+        <div className={styles["date-select"]}>
+          <label>To Date</label>
+          <Select
+            className={styles.reactselect}
+            value={toDate}
+            isClearable={true}
+            isSearchable={true}
+            onChange={toDateChangeHandler}
+            options={toDateOptions.map((monthItem, indx) => {
+              return { value: indx, label: monthItem };
+            })}
+          />
+        </div>
+      </div>
     </div>
   );
 };
