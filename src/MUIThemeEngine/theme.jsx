762ef18dd48c9bc6232949b4bee2956cf36bf74e
Amritanshu <91420206+amritanshu001@users.noreply.github.com>
2024-10-12 16:14:00 +0530

    using usememo correctly

--- src/MUIThemeEngine/theme.jsx
+++ src/MUIThemeEngine/theme.jsx
@@ -4,26 +4,30 @@ import { useSelector } from "react-redux";
 
 const MuiThemeProvider = (props) => {
   const mode = useSelector((state) => state.themeMode.mode);
-  console.log("Current Mode:", mode);
-  const theme = createTheme({
-    components: {
-      MuiAccordion: {
-        styleOverrides: {
-          root: {
-            width: "90%",
-            margin: "auto",
-            marginTop: "0.5rem",
-            fontWeight: "bold",
-            backgroundColor: "#405d27",
-            color: "white",
-          },
-          region: {
-            backgroundColor: "white",
+  // console.log("Current Mode:", mode);
+  const theme = useMemo(
+    () =>
+      createTheme({
+        components: {
+          MuiAccordion: {
+            styleOverrides: {
+              root: {
+                width: "90%",
+                margin: "auto",
+                marginTop: "0.5rem",
+                fontWeight: "bold",
+                backgroundColor: "#405d27",
+                color: "white",
+              },
+              region: {
+                backgroundColor: "white",
+              },
+            },
           },
         },
-      },
-    },
-  });
+      }),
+    [mode]
+  );
 
   return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
 };
