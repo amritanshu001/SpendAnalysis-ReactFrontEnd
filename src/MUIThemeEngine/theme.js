0bbc6881dfc24447e06df8c902dd2642267820a1
Amritanshu <91420206+amritanshu001@users.noreply.github.com>
2024-10-12 15:47:18 +0530

    creating custom theme provider

--- src/MUIThemeEngine/theme.js
+++ src/MUIThemeEngine/theme.js
@@ -1,26 +1,32 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useMemo } from "react";
 
-const theme = createTheme({
-    components:{
-        MuiAccordion:{
-            styleOverrides:{
-                root:{
-                    width: "90%",
-                    margin: "auto",
-                    marginTop: "0.5rem",
-                    fontWeight: "bold",
-                    backgroundColor: "#405d27",
-                    color: "white",
-                    
-                },
-                region: {
-                    backgroundColor: "white",
-                },
-
-            }
+const MuiThemeProvider = (props) => {
+  mode = "light";
+  useMemo(() => {
+    const theme = createTheme({
+      palette: {
+        mode: "light",
+      },
+      components: {
+        MuiAccordion: {
+          styleOverrides: {
+            root: {
+              width: "90%",
+              margin: "auto",
+              marginTop: "0.5rem",
+              fontWeight: "bold",
+              backgroundColor: "#405d27",
+              color: "white",
+            },
+            region: {
+              backgroundColor: "white",
+            },
+          },
         },
-        
-    }
-});
+      },
+    });
+  }, [mode]);
+};
 
 export default theme;
