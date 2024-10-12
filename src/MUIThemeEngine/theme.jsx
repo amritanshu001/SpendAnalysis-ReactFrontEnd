c420bee107a8876692a35907adb22dd13b5748c4
Amritanshu <91420206+amritanshu001@users.noreply.github.com>
2024-10-12 16:11:22 +0530

    removing usememo from theme

--- src/MUIThemeEngine/theme.jsx
+++ src/MUIThemeEngine/theme.jsx
@@ -5,30 +5,26 @@ import { useSelector } from "react-redux";
 const MuiThemeProvider = (props) => {
   const mode = useSelector((state) => state.themeMode.mode);
   console.log("Current Mode:", mode);
-  const theme = useMemo(() => {
-    createTheme({
-      //   palette: {
-      //     mode: "light",
-      //   },
-      components: {
-        MuiAccordion: {
-          styleOverrides: {
-            root: {
-              width: "90%",
-              margin: "auto",
-              marginTop: "0.5rem",
-              fontWeight: "bold",
-              backgroundColor: "#405d27",
-              color: "white",
-            },
-            region: {
-              backgroundColor: "white",
-            },
+  const theme = createTheme({
+    components: {
+      MuiAccordion: {
+        styleOverrides: {
+          root: {
+            width: "90%",
+            margin: "auto",
+            marginTop: "0.5rem",
+            fontWeight: "bold",
+            backgroundColor: "#405d27",
+            color: "white",
+          },
+          region: {
+            backgroundColor: "white",
           },
         },
       },
-    });
-  }, [mode]);
+    },
+  });
+
   return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
 };
 
